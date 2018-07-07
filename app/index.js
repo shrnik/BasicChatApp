f0c983a20d6e9f073efa07720aa2d7b5e236fba8
shrnik <shrenikborad@gmail.com>
2018-07-07 13:57:24 +0530

    added https

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ require('./auth')();
 
 let ioServer = app=>{
     app.locals.chatrooms = [];
-    const server = require('http').Server(app);
+    const server = require('https').Server(app);
     const io  = require('socket.io')(server);
     io.use((socket,next)=>{
         require('./session')(socket.request,{},next);
